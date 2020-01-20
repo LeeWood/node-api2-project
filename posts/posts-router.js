7f9fd6fde9efaee554c180531a4f3d89a826ea07
lee-wood <leemaewood@gmail.com>
2020-01-20 18:36:40 -0500

    post comments searchable by post id, will return error if post does not exist

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -40,4 +40,28 @@ router.get('/:id', (req, res) => {
     });
 });
 
+//GET comments for specified post.
+router.get('/:id/comments', (req, res) => {
+//* need to see if post exists FIRST...then search for comments.
+
+  db.findById(req.params.id)
+    .then(post => {
+      if(post.length > 0) {
+        db.findPostComments(req.params.id)
+          .then(comments => {
+            console.log(comments);
+            res.status(200).json(comments);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist" })
+      } 
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 module.exports = router;
